ae027ebbdb5f512cec2f2f03fe264443c88ebd65
Ramyaveerasekar <36103153+Ramyaveerasekar@users.noreply.github.com>
2018-02-04 16:30:39 +0530

    Add files via upload

---
+++ codekata2.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module4
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+a=input("enter the number")
+if(a%2 == 0):
+     print("even")
+else:
+     print("odd")
