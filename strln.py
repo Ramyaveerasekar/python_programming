6e3c5202b0d38165ea406afb2006acc12c957a79
Ramyaveerasekar <36103153+Ramyaveerasekar@users.noreply.github.com>
2018-02-08 23:25:35 +0530

    Add files via upload

---
+++ strln.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      VINO
+#
+# Created:     08/02/2018
+# Copyright:   (c) VINO 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print('word'[-1])
+word = 'assume'
+print(word[:2])
+print('word'[1:3])
+str='programmer'
+print len(str)
+
+
+
+
