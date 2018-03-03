8e95114a74837bf20c5fb107684871d2c58fd3cf
Ramyaveerasekar <36103153+Ramyaveerasekar@users.noreply.github.com>
2018-03-03 18:19:46 +0530

    Add files via upload

---
+++ largeno.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number a:"))
+b=int(input("enter the number b:"))
+c=int(input("enter the number c:"))
+if(a>b and c):
+    print(a)
+elif(b> c and a):
+    print(b)
+else:
+    print(c)
