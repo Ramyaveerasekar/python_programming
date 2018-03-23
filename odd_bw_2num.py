699fa47f52071532cfd2d4c61cfab7bb4b14c368
Ramyaveerasekar <36103153+Ramyaveerasekar@users.noreply.github.com>
2018-03-23 18:51:37 +0530

    Add files via upload

---
+++ odd_bw_2num.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      VINO
+#
+# Created:     23/03/2018
+# Copyright:   (c) VINO 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+num1=int(input())
+num2=int(input())
+for i in range(num1,num2+1):
+    if(i%2!=0):
+        print(i)
